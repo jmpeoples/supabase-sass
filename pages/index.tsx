f50001247c1cbaa2dd1d450404cfbbaa3a67e2d5
jmpeoples <jmpeoples.88@gmail.com>
2022-10-05 20:37:44 -0500

    Add Third party auth

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import { supabase } from "../utils/supabase";
 
 const Home: NextPage = ({lessons}:any) => {
  
-
+ console.log(supabase.auth.user());
  
   return (
     <div className="w-full max-w-3xl mx-auto my-16 px-2">
