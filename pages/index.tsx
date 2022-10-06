edc49682ddd6f387fd7e406304157d7a6dfe226e
jmpeoples <jmpeoples.88@gmail.com>
2022-10-05 19:59:54 -0500

    setup database connection

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,33 @@
 import type { NextPage } from 'next'
 
-const Home: NextPage = () => {
+import { supabase } from "../utils/supabase";
+
+const Home: NextPage = (lessons) => {
+ 
+
+  
+  let learning = Object.values(lessons);
+  let newLearning = learning.flatMap((learn) => learn);
+
+  console.log("learing",  newLearning);
+ 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      working
+      {newLearning.map((lesson:any) => (
+        <p key={lesson.id}>{lesson.title}</p>
+      ))}
     </div>
   )
+}
+
+export const getStaticProps = async () => {
+  const { data: lessons } = await supabase.from('lesson').select('*');
+
+  return {
+    props: {
+      lessons
+    }
+  }
 }
 
 export default Home
