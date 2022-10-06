7b15ee7f07e574d862f959604356021d6102cb4f
jmpeoples <jmpeoples.88@gmail.com>
2022-10-05 20:23:30 -0500

    Query single record from Supabase

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,18 @@
 import type { NextPage } from 'next'
+import Link from "next/link";
 
 import { supabase } from "../utils/supabase";
 
-const Home: NextPage = (lessons) => {
+const Home: NextPage = ({lessons}:any) => {
  
 
-  
-  let learning = Object.values(lessons);
-  let newLearning = learning.flatMap((learn) => learn);
-
-  console.log("learing",  newLearning);
  
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      {newLearning.map((lesson:any) => (
-        <p key={lesson.id}>{lesson.title}</p>
+    <div className="w-full max-w-3xl mx-auto my-16 px-2">
+      {lessons.map((lesson:any) => (
+        <Link key={lesson.id} href={`/${lesson.id}`}>
+          <a className='p-8 h-40 mb-4 rounded shadow text-xl flex'>{lesson.title}</a>
+          </Link>
       ))}
     </div>
   )
