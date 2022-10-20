681c098db9bd7e997c5703bfc91b925e250af930
jmpeoples <jmpeoples.88@gmail.com>
2022-10-19 21:15:05 -0500

    Cancel subscription

---
+++ pages/api/portal.js
@@ -0,0 +1,38 @@
+import {supabase} from '../../utils/supabase'
+import cookie from "cookie";
+import initStripe from "stripe";
+
+const handler = async(req, res) =>{
+    // figure who are users are in the api route set auth cookie
+  const {user} = await supabase.auth.api.getUserByCookie(req);
+
+  if(!user){
+    return res.status(401).send('Unauthorized');
+  }
+
+  //before making request to supabase extract token
+  const token = cookie.parse(req.headers.cookie)["sb-access-token"];
+
+//https://supabase.com/docs/reference/javascript/auth-setauth
+ supabase.auth.setAuth(token);
+  
+  //to initiate our payment with stripe need stripe profile
+
+  const {data: {stripe_customer}} = await supabase
+  .from('profile')
+  .select('stripe_customer')
+  .eq('id', user.id)
+  .single()
+
+  const stripe = initStripe(process.env.STRIPE_SECRET_KEY);
+
+  const session = await stripe.billingPortal.sessions.create({
+    customer: stripe_customer,
+    return_url: 'http://localhost:3000/dashboard'
+      })
+      res.send({
+        url: session.url
+      })
+}
+
+export default handler;
---
+++ pages/privacy.js
@@ -0,0 +1,12 @@
+const Privacy = () => {
+    return(
+ 
+    <div>
+ <h1>Privacy</h1>
+   
+    </div>
+     
+    )
+ }
+ 
+ export default Terms
